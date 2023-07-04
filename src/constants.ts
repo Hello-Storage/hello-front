4bab3d4f5653f483194c33d41b13b6a6e5df3abf
Alexander Baikalov <asederado@gmail.com>
2023-07-04 16:42:03 +0200

    added ipv4 localnet addres

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 //PROD: https://ounn.space
 //DEV: http://185.166.212.43:6969
-export const baseUrl = "http://localhost:6969" //replace with specific domain url
+export const baseUrl = "http://10.254.16.55:6969" //replace with specific domain url
 export const baseName = "/metaauth"
