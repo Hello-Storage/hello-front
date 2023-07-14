10670a3ab41f75cb2bc1273abc6ba02aa21b48ab
Alexander Baikalov <asederado@gmail.com>
2023-07-14 15:22:33 +0200

    switched to domain backend API endpoint URL

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@
 //DEV: http://185.166.212.43:6969
 //export const baseUrl = "http://localhost:6969" //replace with specific domain url
 //export const baseUrl = "https://ounn.space" //replace with specific domain url
-export const baseUrl = "http://localhost:6969" //replace with specific domain url
+export const baseUrl = "https://ounn.space" //replace with specific domain url
 export const baseName = "/metaauth"
