9293440428ca9379e3116ee8104dbe56577f56c5
Alexander Baikalov <asederado@gmail.com>
2023-07-18 17:09:14 +0200

    replaced baseUrl to the DNS one

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 //PROD: https://ounn.space
 //DEV: http://185.166.212.43:6969
 //LOCAL: "http://localhost:6969" //replace with specific domain url
-export const baseUrl = "http://localhost:6969" //replace with specific domain url
+export const baseUrl = "https://ounn.space" //replace with specific domain url
 export const baseName = "/hello-front"
 export const viteBase = "/hello-front/"
