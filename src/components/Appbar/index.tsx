efee6a00bb67cee88c20196ddf5e748754961bbf
eroist <97012368+eroist@users.noreply.github.com>
2023-08-02 14:50:00 -0400

    fix: remove svg warnings

--- src/components/Appbar/index.tsx
+++ src/components/Appbar/index.tsx
@@ -27,9 +27,9 @@ export default function Appbar() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
