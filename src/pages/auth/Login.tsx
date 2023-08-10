5c32b80484265d0aa5b378538121fba0236097df
Borja Soler <borjasolrey@gmail.com>
2023-08-10 09:38:27 +0200

    Login page Buttons and styles improvements

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -4,7 +4,7 @@ import shows from "@images/auth/shows.png";
 
 export default function Login() {
   return (
-    <div className="p-8 h-screen">
+    <div className="p-8 md:h-screen">
       <div className="md:absolute flex items-center gap-2">
         <LogoIcon />
         <label className="text-3xl font-semibold">Hello</label>
@@ -13,11 +13,15 @@ export default function Login() {
       <div className="flex flex-col md:flex-row gap-8 md:gap-0 h-full">
         <div className="flex items-center justify-center flex-1">
           <div className="md:min-w-[400px] mt-12 md:mt-0">
-            <h1 className="text-4xl font-semibold">Welcome 👋</h1>
-            <h3 className="text-lg mt-4">Select your favorite login option</h3>
+            <h1 className="text-4xl font-semibold tracking-tighter">
+              Welcome 👋
+            </h1>
+            <h3 className="text-lg mt-4 tracking-tighter">
+              Select your favorite login option
+            </h3>
 
             <div className="mt-12">
-              <button className="w-full inline-flex items-center justify-center text-white p-3 rounded-xl bg-gradient-to-b from-violet-500 to-violet-700 hover:from-violet-600 hover:to-violet-800">
+              <button className="w-full inline-flex items-center justify-center text-white px-3 py-4 rounded-xl bg-gradient-to-b from-violet-500 to-violet-700 hover:from-violet-600 hover:to-violet-800">
                 Connect with Wallet
               </button>
             </div>
@@ -36,13 +40,13 @@ export default function Login() {
                 <input
                   type="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-xl focus:border-gray-400 focus:outline-none block w-full px-2.5 py-4"
                   placeholder="example@email.com"
                   required
                 />
 
                 <div className="mt-6">
-                  <button className="w-full inline-flex items-center justify-center text-white p-3 rounded-xl bg-gradient-to-b from-green-500 to-green-700 hover:from-green-600 hover:to-green-800">
+                  <button className="w-full inline-flex items-center justify-center text-white px-3 py-4 rounded-xl bg-gradient-to-b from-green-500 to-green-700 hover:from-green-600 hover:to-green-800">
                     Receive a magic link ✨
                   </button>
                 </div>
@@ -50,8 +54,8 @@ export default function Login() {
             </form>
 
             <div className="inline-flex items-center justify-center w-full relative">
-              <hr className="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">
+              <hr className="w-full h-px my-8 bg-gray-200 border-0" />
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2">
                 Or
               </span>
             </div>
@@ -74,7 +78,7 @@ export default function Login() {
         </div>
 
         <div
-          className={`flex items-center flex-1 bg-[url('/public/images/login-bg.jpg')] bg-cover rounded-[40px]`}
+          className={`flex items-center flex-1 bg-[url('/images/login-bg.jpg')] bg-cover rounded-[40px]`}
         >
           <div className="w-full p-6 md:p-0">
             <img className="mx-auto" src={shows} alt="shows" />
