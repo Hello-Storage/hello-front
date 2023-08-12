5b917820c1f28d9e78df217d48b118c7ece691b9
eroist <97012368+eroist@users.noreply.github.com>
2023-08-12 16:38:21 -0700

    feat: add github oauth

--- src/pages/auth/components/GithubLoginButton.tsx
+++ src/pages/auth/components/GithubLoginButton.tsx
@@ -1,8 +1,9 @@
+import { useEffect, useRef, useState } from "react";
 import { Api, setAuthToken } from "api";
 import { GithubIcon } from "components";
 import { GITHUB_CLIENT_ID, GITHUB_REDIRECT_URL } from "config";
 import { useAuth } from "hooks";
-import { useEffect, useState } from "react";
+import OauthPopup from "react-oauth-popup";
 
 const root_url = "https://github.com/login/oauth/authorize?";
 
@@ -13,64 +14,36 @@ const options = {
   //   state: "/login",
 };
 
-function openWindow(url: string, name: string, props: string) {
-  var windowRef = window.open(url, name, props);
-  if (windowRef && !windowRef.opener) {
-    windowRef.opener = self;
-  }
-  windowRef?.focus();
-  return windowRef;
-}
 
 export default function GithubLoginButton() {
-  const [popup, setPopup] = useState<Window | null>(null);
-
-  useEffect(() => {
-    if (popup) {
-      const currentURL = popup.location.href;
-      const params = new URL(currentURL).searchParams;
-      const code = params.get("code");
-      console.log("currentURL", currentURL);
-
-      console.log("code", code);
-
-      //   popup.close();
-    }
-  }, [popup]);
-
-  const onClick = () => {
-    const width = 600;
-    const height = 800;
-    const left = window.screenX + (window.outerWidth - width) / 2;
-    const top = window.screenY + (window.outerHeight - height) / 2.5;
-    const popup = window.open(
-      root_url + new URLSearchParams(options),
-      "_blank",
-      `popup,width=${width},height=${height},left=${left},top=${top}`
+  const onCode = (code: string, params: any) => {
+    console.log("wooooo a code", code);
+    console.log(
+      "alright! the URLSearchParams interface from the popup url",
+      params
     );
-    setPopup(popup);
   };
+  const onClose = () => console.log("closed!");
 
   return (
-    // <OauthPopup
-    //   url="https://github.com/login/oauth/authorize?client_id=c50211fcfde1104ec365"
-    //   onCode={onCode}
-    //   onClose={onClose}
-    //   title={"Login With Github"}
-    //   width={600}
-    //   height={700}
-    // >
-    <>
+    <OauthPopup
+      url={root_url + new URLSearchParams(options)}
+      onCode={onCode}
+      onClose={onClose}
+      title={"Login With Github"}
+      width={600}
+      height={700}
+    >
+    
       <button
-        //   href={root_url + new URLSearchParams(options)}
         className="w-full inline-flex items-center justify-center gap-4 rounded-xl p-3 bg-gray-100 hover:bg-gray-200"
         type="button"
-        onClick={onClick}
+        
       >
         <GithubIcon /> Connect with Github
       </button>
-    </>
+    
 
-    // </OauthPopup>
+    </OauthPopup>
   );
 }
