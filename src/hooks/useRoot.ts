63e805f82e661edcbeee22cbd777b60132551d6f
eroist <97012368+eroist@users.noreply.github.com>
2023-08-15 04:30:05 -0700

    feat: add folder functionality

--- src/hooks/useRoot.ts
+++ src/hooks/useRoot.ts
@@ -1,17 +1,26 @@
+import { useLocation } from "react-router-dom";
 import { Api, RootResponse } from "api";
 import { useAppDispatch } from "state";
 import { fetchContent } from "state/dashboard/actions";
+import { useCallback } from "react";
 
 const useRoot = () => {
   const dispatch = useAppDispatch();
+  const location = useLocation();
 
-  const fetchRootContent = () => {
-    Api.get<RootResponse>("/folder")
+  const fetchRootContent = useCallback(() => {
+    var root = "/folder";
+
+    if (location.pathname.includes("/folder")) {
+      root = "/folder/" + location.pathname.split("/")[2];
+    }
+
+    Api.get<RootResponse>(root)
       .then((res) => {
         dispatch(fetchContent(res.data));
       })
       .catch((err) => {});
-  };
+  }, [location.pathname]);
 
   return {
     fetchRootContent,
