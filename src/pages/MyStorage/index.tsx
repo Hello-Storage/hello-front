00434cddf818cf79e38566170e829a910652c99a
Borja Soler <borjasolrey@gmail.com>
2023-08-23 13:20:41 +0200

    Added card or table list filter button functionality

--- src/pages/MyStorage/index.tsx
+++ src/pages/MyStorage/index.tsx
@@ -59,6 +59,8 @@ export default function Home() {
       file.uid.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const [view, setView] = useState("list");
+
   const onRadioChange = (e: any) => {
     setFilter(e.target.value);
   };
@@ -155,6 +157,7 @@ export default function Home() {
           <div className="inline-flex rounded-md shadow-sm" role="group">
             <button
               type="button"
+              onClick={() => setView("list")}
               className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-1 focus:ring-gray-300 focus:text-blue-700"
             >
               <HiOutlineViewList size={20} />
@@ -162,6 +165,7 @@ export default function Home() {
 
             <button
               type="button"
+              onClick={() => setView("grid")}
               className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-l-0 border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-1 focus:ring-gray-300 focus:text-blue-700"
             >
               <HiOutlineViewGrid size={20} />
@@ -170,56 +174,79 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-1 mt-3">
-        <div className="hidden md:flex flex-col flex-1 max-h-screen">
-          <div className="overflow-auto md:overflow-visible max-h-[calc(100vh-6rem)] custom-scrollbar">
-            <table className="w-full text-sm text-left text-gray-500 table-with-lines">
-              <thead className="text-xs text-gray-700 bg-gray-100">
-                <tr>
-                  <th scope="col" className="p-3 rounded-tl-lg rounded-bl-lg">
-                    Name
-                  </th>
-                  <th scope="col" className="p-1">
-                    CID
-                  </th>
-                  <th scope="col" className="p-1">
-                    Size
-                  </th>
-                  <th scope="col" className="py-1 px-3">
-                    Type
-                  </th>
-                  <th scope="col" className="p-1 whitespace-nowrap">
-                    Last Modified
-                  </th>
-                  <th scope="col" className="rounded-tr-lg rounded-br-lg"></th>
-                </tr>
-              </thead>
-              <Files folders={filteredFolders} files={filteredFiles} />
-            </table>
-          </div>
-          <div className="flex-1" id="right">
-            <ContextMenu targetId="right" />
-          </div>
-        </div>
-
-        <div className="md:hidden grid grid-cols-2 gap-4 pb-16">
-          {/* Card view for smaller screens */}
-          {mystorage?.files.map((v, i) => (
-            <div
-              className="bg-white p-4 rounded-md mb-3 border border-gray-200 shadow-md"
-              key={i}
-            >
-              <div className="flex flex-col items-center gap-3">
-                <div className="p-1 bg-gray-100 rounded-md">
-                  <HiDocumentText className="w-7 h-7" />
-                </div>
-                <div className="font-medium text-gray-900 w-full">{v.name}</div>
-              </div>
-              <div className="flex items-center mt-4 text-xs gap-2">
-                {formatUID(v.uid)} <HiDocumentDuplicate />
-              </div>
+        {view === "list" && (
+          <div className="flex flex-col flex-1 max-h-screen">
+            <div className="overflow-auto md:overflow-visible max-h-[calc(100vh-6rem)] custom-scrollbar">
+              <table className="w-full text-sm text-left text-gray-500 table-with-lines">
+                <thead className="text-xs text-gray-700 bg-gray-100">
+                  <tr>
+                    <th scope="col" className="p-3 rounded-tl-lg rounded-bl-lg">
+                      Name
+                    </th>
+                    <th scope="col" className="p-1">
+                      CID
+                    </th>
+                    <th scope="col" className="p-1">
+                      Size
+                    </th>
+                    <th scope="col" className="py-1 px-3">
+                      Type
+                    </th>
+                    <th scope="col" className="p-1 whitespace-nowrap">
+                      Last Modified
+                    </th>
+                    <th
+                      scope="col"
+                      className="rounded-tr-lg rounded-br-lg"
+                    ></th>
+                  </tr>
+                </thead>
+                <Files folders={filteredFolders} files={filteredFiles} />
+              </table>
             </div>
-          ))}
-        </div>
+            <div className="flex-1" id="right">
+              <ContextMenu targetId="right" />
+            </div>
+          </div>
+        )}
+        {view === "grid" && (
+          <div className="flex flex-wrap justify-between pb-16">
+            {filteredFolders.map((v, i) => (
+              <div className="bg-white p-4 rounded-md mb-3 border border-gray-200 shadow-md w-64 flex flex-col justify-between h-[240px]">
+                <div>
+                  <div className="flex flex-col items-center gap-3">
+                    <div className="p-1 bg-gray-100 rounded-md">
+                      <HiDocumentText className="w-7 h-7" />
+                    </div>
+                    <div className="font-medium text-gray-900 w-full overflow-hidden whitespace-nowrap overflow-ellipsis">
+                      {v.title}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center mt-auto text-xs gap-2">
+                  {formatUID(v.uid)} <HiDocumentDuplicate />
+                </div>
+              </div>
+            ))}
+            {filteredFiles.map((v, i) => (
+              <div className="bg-white p-4 rounded-md mb-3 border border-gray-200 shadow-md w-64 flex flex-col justify-between h-[240px]">
+                <div>
+                  <div className="flex flex-col items-center gap-3">
+                    <div className="p-1 bg-gray-100 rounded-md">
+                      <HiDocumentText className="w-7 h-7" />
+                    </div>
+                    <div className="font-medium text-gray-900 w-full overflow-hidden whitespace-nowrap overflow-ellipsis">
+                      {v.name}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center mt-auto text-xs gap-2">
+                  {formatUID(v.uid)} <HiDocumentDuplicate />
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       {/*Add buttons here */}
       <div className="flex justify-between items-center mt-3">
