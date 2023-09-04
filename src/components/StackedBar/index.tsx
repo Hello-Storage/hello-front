78fec6c94ffd51ed9fa41a80a2720d134d5da0e5
eroist <97012368+eroist@users.noreply.github.com>
2023-09-04 08:06:03 -0500

    feat: stacked bar component

---
+++ src/components/StackedBar/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface StackedBarProps {
+  data: {
+    color: string;
+    percent: number;
+  }[];
+}
+
+export default function StackedBar({ data }: StackedBarProps) {
+  return (
+    <span className="bg-gray-100 flex overflow-hidden h-5 rounded-md gap-1 mt-2">
+      {data.map((v, i) => (
+        <span style={{ backgroundColor: v.color, width: `${v.percent}%` }} />
+      ))}
+    </span>
+  );
+}
