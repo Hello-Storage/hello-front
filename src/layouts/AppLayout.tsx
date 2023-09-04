290147032ad5b32d960eb9c28276900ab42def34
Borja Soler <borjasolrey@gmail.com>
2023-09-04 16:23:05 +0200

    mobile sidebar and list of items reduced for the mobile view

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -4,6 +4,7 @@ import Sidebar from "components/Sidebar";
 import Appbar from "components/Appbar";
 import { SearchContext } from "../contexts/SearchContext";
 import { FiMenu } from "react-icons/fi";
+import LogoHello from "@images/beta.png";
 
 export default function AppLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -13,18 +14,25 @@ export default function AppLayout() {
     <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
       <div className="flex h-screen flex-col justify-between">
         {!sidebarOpen && (
-          <button
-            className="md:hidden right-4 top-4 absolute"
-            onClick={() => setSidebarOpen(true)}
-          >
-            <div className="bg-white p-1 border rounded-xl">
-              <FiMenu size={24} />
+          <div className="flex items-center justify-between sticky px-5 py-2 w-full bg-gray-100 md:hidden">
+            <div className="flex items-center gap-3">
+              <label className="text-xl font-semibold font-[Outfit]">
+                Hello.storage
+              </label>
+              <img src={LogoHello} alt="beta" className="w-10 h-5" />
             </div>
-          </button>
+            <button onClick={() => setSidebarOpen(true)}>
+              <div className="bg-white p-1 border rounded-xl">
+                <FiMenu size={24} />
+              </div>
+            </button>
+          </div>
         )}
         <div className="flex flex-grow">
           <div
-            className={`px-6 py-4 ${sidebarOpen ? "block" : "hidden"} md:block`}
+            className={`w-full md:w-72 ${
+              sidebarOpen ? "block" : "hidden"
+            } md:block`}
           >
             <Sidebar setSidebarOpen={setSidebarOpen} />
           </div>
