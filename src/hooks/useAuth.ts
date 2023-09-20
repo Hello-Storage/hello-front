3021900ed43cfc030041af6e5bcb6c314bbc9074
Alexander Baikalov <asederado@gmail.com>
2023-09-20 03:00:03 +0200

    added account type to email otp verify

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -84,6 +84,7 @@ const useAuth = () => {
         code,
       })
       setAuthToken(result.data.access_token);
+      setAccountType(AccountType.Mail)
       load()
       return true;
     } catch (error) {
