6024b03408db3dcdcea7498920d832b2f39948a6
Alexander Baikalov <asederado@gmail.com>
2023-09-25 13:11:23 +0200

    added dropzone again

--- src/pages/MyStorage/components/Dropzone.tsx
+++ src/pages/MyStorage/components/Dropzone.tsx
@@ -34,7 +34,7 @@ export default function Dropzone() {
   return (
     <div
       className={[
-        "flex flex-col items-center justify-center p-8 border-2 rounded-sm border-dashed bg-gray-50 outline-none mb-5 md:hidden",
+        "flex-col items-center justify-center p-8 border-2 rounded-sm border-dashed bg-gray-50 outline-none mb-5 hidden sm:flex",
         `${getColor(isFocused, isDragAccept, isDragReject)}`,
       ].join(" ")}
       {...getRootProps()}
