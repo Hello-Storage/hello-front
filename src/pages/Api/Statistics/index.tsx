c6dd42690ca658981022d3ade79f057c6d43b562
Alexander Baikalov <asederado@gmail.com>
2023-10-17 12:55:07 +0200

    remove stats

--- src/pages/Api/Statistics/index.tsx
+++ src/pages/Api/Statistics/index.tsx
@@ -111,8 +111,7 @@ const Statistics = () => {
     const fetchData = () => {
         // Esta URL debe ser la ruta de tu backend
 
-        const apiUrl = import.meta.env.VITE_API_ENDPOINT + "/statistics"
-
+        const apiUrl = "http://localhost:8080"
         axios
             .get(apiUrl)
             .then((response) => {
