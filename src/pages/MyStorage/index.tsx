b35f180d1d25aab107932bac05ce934f3fa1c492
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-01-25 12:12:05 -0500

    fix: title creation bug (unencrypted folder name)

--- src/pages/MyStorage/index.tsx
+++ src/pages/MyStorage/index.tsx
@@ -163,14 +163,6 @@ export default function Home() {
     }
   }
 
-
-  useEffect(() => {
-    //fetchContent().then(() => {
-    //  setLoading(false);
-    //});
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [folders.length]);
-
   useEffect(() => {
     fetchContent().then(() => {
       setLoading(false);
