204d36e922e5bdb286272a319ed9dd6e0504876a
Onetti01 <113169413+Onetti01@users.noreply.github.com>
2024-01-29 22:27:18 +0100

    fix:searchbar

--- src/components/Appbar/index.tsx
+++ src/components/Appbar/index.tsx
@@ -91,7 +91,7 @@ const Appbar: FunctionComponent<AppbarProps> = ({ onSearchChange }) => {
             <input
               type="search"
               id="default-search"
-              className={"block w-full py-2.5 pl-10 pr-4 text-sm rounded-2xl text-gray-900 border focus:border-gray-400 focus:outline-none xl:w-4/5"
+              className={"block w-full py-2.5 pl-10 pr-4 text-sm rounded-2xl text-gray-900 border focus:border-gray-400 focus:outline-none "
                 + (theme === Theme.DARK ? " dark-theme3" : " border-gray-200 bg-white")}
               placeholder="Search your space"
               required
