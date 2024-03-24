561bd39f36fe54d2b9991ee880dd5bd639090128
Alexander <asederado@gmail.com>
2024-03-24 15:31:29 +0100

    add data fetching and reference links to snapshots

--- src/pages/Snapshots/components/SnapshotContainer.tsx
+++ src/pages/Snapshots/components/SnapshotContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiClipboardCopy } from "react-icons/hi";
 import { toast } from "react-toastify";
 
 interface SnapshotContainerProps {
@@ -15,10 +16,10 @@ export const SnapshotContainer: React.FC<SnapshotContainerProps> = ({
   cid,
 }) => {
 
-    function copyToClipboard(text: string) {
-        navigator.clipboard.writeText(text)
-        toast.success("Copied to clipboard")
-    }
+  function copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text)
+    toast.success("Copied to clipboard")
+  }
 
   return (
     <div className="snapshots-p-container">
@@ -27,22 +28,22 @@ export const SnapshotContainer: React.FC<SnapshotContainerProps> = ({
         <tbody>
           <tr>
             <td className="p-2">transaction_id</td>
-            <td className="p-2 cursor-pointer" onClick={()=>{copyToClipboard(transaction_id)}}>{transaction_id}</td>
+            <td className="flex flex-row m-2 p-2 cursor-pointer" onClick={() => { copyToClipboard(transaction_id) }}>{transaction_id}<HiClipboardCopy className="ml-2 w-50 h-50" /></td>
           </tr>
           <tr>
             <td className="p-2">owner</td>
-            <td className="p-2 cursor-pointer" onClick={()=>{copyToClipboard(owner)}}>{owner}</td>
+            <td className="p-2 cursor-pointer" onClick={() => { copyToClipboard(owner) }}>{owner}</td>
           </tr>
           <tr>
             <td className="p-2">cid</td>
-            <td className="p-2 cursor-pointer" onClick={()=>{copyToClipboard(cid)}}>{cid}</td>
+            <td className="p-2 cursor-pointer" onClick={() => { copyToClipboard(cid) }}>{cid}</td>
           </tr>
         </tbody>
       </table>
       <section className="w-full flex flex-col md:flex-row justify-center items-center gap-2 mt-2">
-        <button>Arweave</button>
-        <button>IPFS</button>
-        <button>View On Chain Explorer</button>
+        <a target="_blank" href={`https://arweave.net/${transaction_id}`}>  <button>Arweave</button></a>
+        <a target="_blank" href={`https://ipfs.io/ipfs/${cid}`}><button>IPFS</button></a>
+        <a target="_blank" href={`https://viewblock.io/arweave/tx/${transaction_id}`}><button>View On Chain Explorer</button></a>
       </section>
     </div>
   );
