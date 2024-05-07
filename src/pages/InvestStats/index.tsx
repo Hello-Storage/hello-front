17ebe92ae9b760476e590b662e08972f312abae2
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2024-05-07 14:55:27 -0500

    Web responsive Invest Stats

--- src/pages/InvestStats/index.tsx
+++ src/pages/InvestStats/index.tsx
@@ -11,7 +11,7 @@ interface InvestAccount {
 }
 
 
-export default function InvestStats(){
+export default function InvestStats() {
 
     const apiUrl = import.meta.env.VITE_API_ENDPOINT
     const [investAccounts, setInvestAccounts] = React.useState([])
@@ -20,49 +20,48 @@ export default function InvestStats(){
     const [filterAccount, setFilterAccount] = React.useState<InvestAccount[]>([])
 
 
-    React.useEffect(()=>{
+    React.useEffect(() => {
 
-        async function fetchData(){
-            const {data} = await axios.get(`${apiUrl}/invest`) 
+        async function fetchData() {
+            const { data } = await axios.get(`${apiUrl}/invest`)
             setInvestAccounts(data.message)
-            if(!allAccounts){
+            if (!allAccounts) {
                 const response = await axios.get(`${apiUrl}/invest/all`)
                 setAllAccounts(response.data.message)
             }
-            
+
         }
 
         fetchData()
-    },[])
+    }, [])
 
 
     return (
-        <div className="min-h-screen pt-[100px] flex items-center justify-start flex-col gap-5 box-border">
+
+        <div className="min-h-screen pt-[100px] flex flex-col gap-5 box-border">
 
             <div className="absolute top-5 left-5 flex items-center">
                 <h1 className="text-2xl font-semibold text-black flex items-center font-[outfit]" title="hello app"> {"hello.app"} </h1>
-                <img src="https://hello.app/assets/beta-e8ce8431.png" alt="hello beta" className="h-[22px] w-auto ml-3"/>
+                <img src="https://hello.app/assets/beta-e8ce8431.png" alt="hello beta" className="h-[22px] w-auto ml-3" />
             </div>
-            
-            <div className="relative h-[40%] w-full px-10  box-border">
-                <button onClick={()=>{setFilterAccount([])}} className="h-[40px] bg-blue-100 px-3 border-0 rounded-lg absolute top-[-45px] right-[50px] transition-all hover:bg-blue-300 hover:scale-[1.05]">Reset</button>
-                <div className="h-full w-full border rounded-lg bg-blue-100 p-5 overflow-y-scroll">
-                    
-                        <ul className="h-[20%] w-full flex justify-between">
-                            <li className="w-[30%] flex justify-start">Code</li>
-                            <li className="w-[30%] flex justify-start">Email</li>
-                            <li className="w-[30%] flex justify-start">Social network</li>
-                        </ul>
-                    
-                    <div className="h-min w-full flex flex-col justify-start items-start gap-2">
-                        
-                        {investAccounts?.map((x, index)=>{
-                            const {code, mail, social_network, InvestAccounts} = x
-                            return(
-                                <ul onClick={()=>{setFilterAccount(InvestAccounts)}} key={index} className="h-[20%] w-full flex justify-between p-3 cursor-pointer border rounded-lg hover:bg-blue-300">
-                                    <li className="w-[28%] flex justify-start">{code}</li>
-                                    <li className="w-[28%] flex justify-start">{mail}</li>
-                                    <li className="w-[28%] flex justify-start">{social_network}</li>
+
+            <div className="relative w-full px-5 sm:px-10 box-border">
+                <button onClick={() => { setFilterAccount([]) }} className="h-[40px] bg-blue-100 px-3 border-0 rounded-lg absolute top-[-45px] right-[50px] transition-all hover:bg-blue-300 hover:scale-[1.05]">Reset</button>
+                <div className="w-full border rounded-lg bg-blue-100 p-5 overflow-y-scroll">
+
+                    <ul className="w-full flex justify-start">
+                        <li className="min-w-[150px] w-[30%] flex justify-start">Code</li>
+                        <li className="min-w-[150px] w-[30%] flex justify-start">Social network</li>
+                    </ul>
+
+                    <div className="w-full flex flex-col justify-start items-start gap-2">
+
+                        {investAccounts?.map((x, index) => {
+                            const { code, social_network, InvestAccounts } = x
+                            return (
+                                <ul onClick={() => { setFilterAccount(InvestAccounts) }} key={index} className="w-full flex justify-start p-3 cursor-pointer border rounded-lg hover:bg-blue-300">
+                                    <li className="min-w-[150px] w-[30%] flex justify-start">{code}</li>
+                                    <li className="min-w-[150px] w-[30%] flex justify-start">{social_network}</li>
                                 </ul>
                             )
                         })}
@@ -70,57 +69,46 @@ export default function InvestStats(){
                 </div>
             </div>
 
-            <div className="w-full px-10 flex justify-end">
-                {filterAccount.length == 0 
+            <div className="w-full px-5 sm:px-10 flex justify-end">
+                {filterAccount.length == 0
                     ? <p>{`Total Accounts: ${allAccounts?.length}`}</p>
-                    :<p>{`Total Accounts by code: ${filterAccount?.length}`}
-                </p>}
+                    : <p>{`Total Accounts by code: ${filterAccount?.length}`}</p>}
             </div>
 
-            <div className="min-h-[40%] h-min w-full px-10  box-border">
-                <div className="h-full w-full border rounded-lg bg-blue-100 p-5 ">
-                    
-                        <ul className="h-[20%] w-full flex justify-between">
-                            <li className="w-[30%] flex justify-start">Ip</li>
-                            <li className="w-[30%] flex justify-start">Date</li>
-                            <li className="w-[30%] flex justify-start">Code</li>
-                        </ul>
-                    
-                    <div className="h-min w-full flex flex-col justify-start items-start gap-2">
+            <div className="min-h-[40%] h-min w-full px-5 sm:px-10 box-border">
+                <div className="w-full border rounded-lg bg-blue-100 p-5 overflow-auto ">
 
+                    <ul className="w-full flex justify-between">
+                        <li className="min-w-[150px] w-[30%] flex justify-start">Ip</li>
+                        <li className="min-w-[150px] w-[30%] flex justify-start">Date</li>
+                        <li className="min-w-[150px] w-[30%] flex justify-start">Code</li>
+                    </ul>
 
-                        
-                        {filterAccount?.length !== 0 ? filterAccount?.map((x, index)=>{
-                            const {ip, created_at, code} = x
-                            return(
-                                <ul key={index} className="h-[20%] w-full flex justify-between p-3 cursor-pointer border rounded-lg hover:bg-blue-300">
-                                    <li className="w-[28%] flex justify-start">{ip}</li>
-                                    <li className="w-[28%] flex justify-start">{created_at.split("T")[0]}</li>
-                                    <li className="w-[28%] flex justify-start">{code}</li>
+                    <div className="w-full flex flex-col justify-start items-start gap-2">
+                        {filterAccount?.length !== 0 ? filterAccount?.map((x, index) => {
+                            const { ip, created_at, code } = x
+                            return (
+                                <ul key={index} className="w-full flex justify-between p-3 cursor-pointer border rounded-lg hover:bg-blue-300">
+                                    <li className="min-w-[150px] w-[28%] flex justify-start">{ip}</li>
+                                    <li className="min-w-[150px] w-[28%] flex justify-start">{created_at.split("T")[0]}</li>
+                                    <li className="min-w-[150px] w-[28%] flex justify-start">{code}</li>
                                 </ul>
                             )
                         })
-                        :allAccounts?.map((x, index)=>{
-                            const {ip, created_at, code} = x
-                            return(
-                                <ul key={index} className="h-[20%] w-full flex justify-between p-3 cursor-pointer border rounded-lg hover:bg-blue-300">
-                                    <li className="w-[28%] flex justify-start">{ip}</li>
-                                    <li className="w-[28%] flex justify-start">{created_at.split("T")[0]}</li>
-                                    <li className="w-[28%] flex justify-start">{code}</li>
-                                </ul>
-                            )
-                        })
-                        }
+                            : allAccounts?.map((x, index) => {
+                                const { ip, created_at, code } = x
+                                return (
+                                    <ul key={index} className="w-full flex justify-between p-3 cursor-pointer border rounded-lg hover:bg-blue-300">
+                                        <li className="min-w-[150px] w-[28%] flex justify-start">{ip}</li>
+                                        <li className="min-w-[150px] w-[28%] flex justify-start">{created_at.split("T")[0]}</li>
+                                        <li className="min-w-[150px] w-[28%] flex justify-start">{code}</li>
+                                    </ul>
+                                )
+                            })}
                     </div>
                 </div>
             </div>
-            
-            
-            
-
         </div>
+
     )
 }
-
-
-{/* <div class="grid items-center justify-center max-w-screen-xl grid-cols-1 mx-2 xl:mx-auto"><div class="flex flex-col items-center justify-center gap-3 p-3 mt-3 bg-blue-100 border rounded-lg">Total Users<label class="block font-semibold text-black text-1x8">0</label><div style="height: 230px; width: 100%;"><canvas role="img" height="230" width="730" style="box-sizing: border-box; display: block; height: 230px; width: 730px;"></canvas></div></div></div> */}
